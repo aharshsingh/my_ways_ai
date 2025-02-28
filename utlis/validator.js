e4245b0608fe60dd03c9ea1e9a1da417a905430d
aharshsingh <aharshsingh25@gmail.com>
2025-02-28 16:16:06 +0530

    question

--- utlis/validator.js
+++ utlis/validator.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 
 export const userSignUp = Joi.object({
-    name:      Joi.string().alphanum().required(),
+    name:      Joi.string().required(),
     email:     Joi.string().email().required(),
     password:  Joi.string().required()
 })
