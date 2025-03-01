2945fffa648ca2dbeb204c691d152b3ad8863dbd
aharshsingh <aharshsingh25@gmail.com>
2025-03-01 23:04:40 +0530

    Text-to-speech

--- utlis/addQuestion.js
+++ utlis/addQuestion.js
@@ -1,8 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export async function addQuestion(testId, questionText){
+export async function addQuestion(Id, questionText){
     try {
+        const testId = parseInt(Id);
         return await prisma.question.create({
             data: {testId, questionText}
         })
