7f885738dc630be77363b32e70cc3a0c3b0da431
Lovedeep <lovedeep24253@gmail.com>
2025-04-24 15:15:31 +0530

    added 404 page

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NotFoundPage } from "@/components/ui/404-page-not-found"
+export default function NotFound() {
+  return (
+    <div>
+      <div className="w-full">
+        <NotFoundPage/>
+        </div>
+    </div>
+  )
+}
