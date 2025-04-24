56b5722c789466ff0cbac9287dde22060d513dbe
Lovedeep <lovedeep24253@gmail.com>
2025-04-24 15:26:54 +0530

    fixed 404 page

--- components/ui/404-page-not-found.jsx
+++ components/ui/404-page-not-found.jsx
@@ -1,20 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import Link from 'next/link';
+import Link from "next/link";
 
 export function NotFoundPage() {
   return (
-    (<section
-      className="bg-white font-serif min-h-screen flex items-center justify-center">
+    <section className="bg-white font-serif min-h-screen flex items-center justify-center">
       <div className="container mx-auto">
         <div className="flex justify-center">
           <div className="w-full sm:w-10/12 md:w-8/12 text-center">
             <div
               className="bg-[url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif)] h-[250px] sm:h-[350px] md:h-[400px] bg-center bg-no-repeat bg-contain"
-              aria-hidden="true">
-              <h1
-                className="text-center text-black text-6xl sm:text-7xl md:text-8xl pt-6 sm:pt-8">
+              aria-hidden="true"
+            >
+              <h1 className="text-center text-black text-6xl sm:text-7xl md:text-8xl pt-6 sm:pt-8">
                 404
               </h1>
             </div>
@@ -26,17 +25,15 @@ export function NotFoundPage() {
               <div className="mb-6 text-black sm:mb-5">
                 <p>The page you&apos;re looking for doesn&apos;t exist.</p>
               </div>
-              <Link href={"/"}>
-              <Button
-                variant="default"
-                className="my-5 bg-[#7E22CE]">
-                Go to Home
-              </Button>
+              <Link href="/">
+                <Button variant="default" className="my-5 bg-[#7E22CE]">
+                  Go to Home
+                </Button>
               </Link>
             </div>
           </div>
         </div>
       </div>
-    </section>)
+    </section>
   );
 }
