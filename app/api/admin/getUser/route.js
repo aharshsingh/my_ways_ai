2cccfb92afc2ec82187e3140ebab76a11e2f7136
Aharsh Singh <103747389+aharshsingh@users.noreply.github.com>
2025-04-30 00:06:31 +0530

    fix: Update route.js

--- app/api/admin/getUser/route.js
+++ app/api/admin/getUser/route.js
@@ -5,7 +5,7 @@ import { connectToDatabase } from "@/lib/mongodb";
 export async function GET(req){
     try {
         await connectToDatabase();
-        let users = await User.find().select("userName email attemptedTest createdAt");
+        let users = await User.find().select("userName email attemptedTest createdAt isSuspended");
         const userInfo = users.map((user) => {
             const plainUser = user.toObject();
             return {
