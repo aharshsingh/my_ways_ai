ada2649c1edd8898a9a1402ed582df6a941a9360
aharshsingh <aharshsingh25@gmail.com>
2025-05-30 00:09:59 +0530

    fix: Attempted Test API

---
+++ app/api/attemptedTest/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import Test from '@/lib/models/Test';
+import { connectToDatabase } from "@/lib/mongodb";
+export async function POST(req, {params}){
+    try {        
+        await connectToDatabase();
+        const body = await req.json();
+        const { testIdArray } = body;
+        let result = [];
+        for(let i = 0; i < testIdArray.size(); i++){
+            const response = await Test.findOne({_id: testIdArray[i]}).select("_id testName createdAt numOfQuestion duration score");
+            result.push(response);
+        }
+        return NextResponse(result, {status: 200})
+    } catch (error) {
+        return NextResponse({"error": "Internal Server error"}, {status: 500})
+    }
+}
